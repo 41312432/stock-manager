3955db90bd44e9f5d5814306d0080b02e482f2c2
Wansik Jo <jws5327@ajou.ac.kr>
2022-01-04 13:55:36 +0900

    FEAT: Update section top date using moment
    not Date(), using moment() for day locale(ko) date format

--- src/components/top/top.jsx
+++ src/components/top/top.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./top.scss";
+import moment from "moment";
+import "moment/locale/ko";
 import Moment from "react-moment";
 
 const Top = (props) => {
-  const nowTime = Date.now();
+  const nowTime = moment();
 
   return (
     <header className="top">
       <p>아이템 종류</p>
-      <Moment format="MM월 DD일" withTitle>
+      <Moment format="MMM DDD dddd" withTitle>
         {nowTime}
       </Moment>
     </header>
