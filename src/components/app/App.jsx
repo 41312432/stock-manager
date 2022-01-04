73700333646d9578d1587665cf7a45b66b7b1c85
Wansik Jo <jws5327@ajou.ac.kr>
2022-01-04 22:24:32 +0900

    FEAT: Implement Footer

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.scss";
 import Header from "../header/header";
 import TabContainer from "../tab/tab_container";
+import Footer from "../footer/footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <TabContainer />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./footer.scss";
+
+const Footer = (props) => (
+  <footer class="footer">
+    <div class="divider"></div>
+    <div class="container">
+      <div>
+        <p>
+          <strong>줄리브 원주혁신도시점</strong>
+          <br />
+          📞 050-6769-1967
+          <br />
+          🗺 강원도 원주시 반곡동 1886-2 1층 109호
+          <br /> (반곡동, 골든스퀘어1차)
+        </p>
+      </div>
+      <div>
+        <p>
+          Developed by <strong>Jo Wansik</strong>
+          <br />
+          📝<a href="https://41312432.netlify.app">devLog </a>
+          / 📧 jws5327@ajou.ac.kr
+          <br />
+          <a href="https://github.com/41312432/stock-manager.">git repo</a>
+        </p>
+      </div>
+      <div>
+        <p>
+          © 2021~ <br />
+          github@41321432
+          <br />
+          Cafe Julieve Wonju
+        </p>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
