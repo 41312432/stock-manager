ff47b0554404310cd522f4a3a46a64ff92d4fdd8
Wansik Jo <jws5327@ajou.ac.kr>
2022-01-20 22:53:04 +0900

    FEAT: Update tab top title

--- src/components/top/top.jsx
+++ src/components/top/top.jsx
@@ -4,13 +4,13 @@ import moment from "moment";
 import "moment/locale/ko";
 import Moment from "react-moment";
 
-const Top = (props) => {
+const Top = ({ storageType }) => {
   const nowTime = moment();
 
   return (
     <header className="top">
-      <p>아이템 종류</p>
-      <Moment format="MMM DDD dddd" withTitle>
+      <p>{storageType}</p>
+      <Moment format="MMM DDD dddd hh:mm" withTitle>
         {nowTime}
       </Moment>
     </header>
