9045df621e05ae8fd16289ba1ed24cf199531a35
Wansik Jo <jws5327@ajou.ac.kr>
2022-02-24 17:53:31 +0900

    FEAT: Add modal ItemGroupMonitor to ItemGroup

--- src/components/item_group/item_group.jsx
+++ src/components/item_group/item_group.jsx
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from "react";
 import ReactTooltip from "react-tooltip";
+import ReactModal from "react-modal";
 import Moment from "react-moment";
 import "./item_group.scss";
 import moment from "moment";
+import ItemGroupMonitor from "../item_group_monitor/item_group_monitor";
 
 const ItemGroup = ({ item, itemName, properties }) => {
+  const [showModal, setShowModal] = useState(false);
   const [expClass, setExpClass] = useState("");
   const [icons, setIcons] = useState([]);
+
+  const handleOpenEditor = () => {
+    setShowModal(true);
+  };
+  const handleCloseEditor = () => {
+    setShowModal(false);
+  };
 
   useEffect(() => {
     const src = `../../../icon/${itemName}.png`;
@@ -32,8 +42,6 @@ const ItemGroup = ({ item, itemName, properties }) => {
       temp.push(<p> x {remain}</p>);
     }
 
-    console.log(temp);
-
     setIcons(temp);
   }, []);
 
@@ -53,38 +61,74 @@ const ItemGroup = ({ item, itemName, properties }) => {
   }, []);
 
   return (
-    <div className={expClass} data-tip="React-tooltip">
-      {icons}
-      <ReactTooltip place="top" type="info" effect="float">
-        <ul>
-          <li>
-            생성 날짜 :{" "}
-            <Moment format="MMM Do dddd" locale="ko">
-              {item.createDate}
-            </Moment>
-          </li>
-          <li>
-            폐기 날짜 :{" "}
-            <Moment
-              add={{ days: properties.expDate }}
-              format="MMM Do dddd"
-              locale="ko"
-            >
-              {item.createDate}
-            </Moment>
-          </li>
-          <li>
-            폐기 :{" "}
-            <Moment fromNow locale="ko">
-              {moment(item.createDate)
-                .add(properties.expDate + 1, "days")
-                .endOf("day")
-                .format("YYYY-MM-DD")}
-            </Moment>
-          </li>
-        </ul>
-      </ReactTooltip>
-    </div>
+    <>
+      <div
+        className={expClass}
+        data-tip="React-tooltip"
+        onClick={handleOpenEditor}
+      >
+        {icons}
+        <ReactTooltip place="top" type="info" effect="float">
+          <ul>
+            <li>
+              생성 날짜 :{" "}
+              <Moment format="MMM Do dddd" locale="ko">
+                {item.createDate}
+              </Moment>
+            </li>
+            <li>
+              폐기 날짜 :{" "}
+              <Moment
+                add={{ days: properties.expDate }}
+                format="MMM Do dddd"
+                locale="ko"
+              >
+                {item.createDate}
+              </Moment>
+            </li>
+            <li>
+              폐기 :{" "}
+              <Moment fromNow locale="ko">
+                {moment(item.createDate)
+                  .add(properties.expDate + 1, "days")
+                  .endOf("day")
+                  .format("YYYY-MM-DD")}
+              </Moment>
+            </li>
+          </ul>
+        </ReactTooltip>
+      </div>
+      <ReactModal
+        isOpen={showModal}
+        onRequestClose={handleCloseEditor}
+        style={{
+          overlay: {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+          },
+          content: {
+            margin: "auto",
+            border: "2px solid #000",
+            borderRadius: "15px",
+            background: "#fff",
+            overflow: "auto",
+            width: "75%",
+            height: "60%",
+            WebkitOverflowScrolling: "touch",
+            padding: "5px 20px",
+          },
+        }}
+      >
+        <ItemGroupMonitor />
+      </ReactModal>
+    </>
   );
 };
 
---
+++ src/components/item_group_monitor/item_group_monitor.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./item_group_monitor.scss";
+
+const ItemGroupMonitor = (props) => {
+  return <div></div>;
+};
+
+export default ItemGroupMonitor;
