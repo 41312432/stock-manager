30e710efade9799ccfa2033a0b563f97f106db26
Wansik Jo <jws5327@ajou.ac.kr>
2022-02-24 13:58:22 +0900

    FEAT: Update moment to react-moment and apply locale

--- src/components/top/top.jsx
+++ src/components/top/top.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import "./top.scss";
-import moment from "moment";
 import "moment/locale/ko";
 import Moment from "react-moment";
 
 const Top = ({ storageType }) => {
-  const nowTime = moment();
-
   return (
     <header className="top">
       <p>{storageType}</p>
-      <Moment format="MMM DDD dddd hh:mm" withTitle>
-        {nowTime}
-      </Moment>
+      <Moment format="MMM DD dd hh:mm" locale="ko" />
     </header>
   );
 };
